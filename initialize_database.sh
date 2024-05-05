bddbe8dc2359145b2810e018ddb5702f07c19a82
Brent Crosby <bcrosby3@cs.sierracollege.edu>
2024-05-05 14:50:28 -0700

    Add script to initialize SQLite Database\n\n This script initializes a database with a table to store the animal inventory info if it does not already exist.

---
+++ initialize_database.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Initialize Database Script
+
+# Database filename
+DB_FILE="animal_inventory.db"
+
+# Check if database already exists
+if [ -f "$DB_FILE" ]; then
+	echo "Database File already exists. Skipping Initialization."
+else
+	sqlite3 animal_inventory.db << EOF
+
+	CREATE TABLE IF NOT EXISTS animals (
+		id INTEGER PRIMARY KEY,
+		name TEXT NOT NULL,
+		species TEXT NOT NULL,
+		breed TEXT,
+		sex CHAR(1),
+		weight FLOAT,
+		age INT,
+		birth DATE,
+		death DATE
+		);
+
+	.print "Animal Inventory Database Initialized.\n"
+
+EOF
+fi
